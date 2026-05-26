3786186b04c7e890d383d5f6d1bf423d05846753
ojask14 <ojas.14kulkarni@gmail.com>
2026-05-27 00:06:31 +0530

    Week 8 fixed bugs and deployed project

--- chat-application/src/app/app.spec.ts
+++ chat-application/src/app/app.spec.ts
@@ -1,23 +1,24 @@
 import { TestBed } from '@angular/core/testing';
-import { App } from './app';
+import { AppComponent } from './app';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
-describe('App', () => {
+describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [App],
+      imports: [AppComponent, FormsModule, CommonModule],
     }).compileComponents();
   });
 
   it('should create the app', () => {
-    const fixture = TestBed.createComponent(App);
+    const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
     expect(app).toBeTruthy();
   });
 
-  it('should render title', async () => {
-    const fixture = TestBed.createComponent(App);
-    await fixture.whenStable();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, week1-angular-project');
+  it(`should have as title 'chat-application'`, () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.componentInstance;
+    expect((app as any).title).toEqual('chat-application');
   });
 });
